afe9a8ed2b8acf3538017b60b3e7785bfce7f4a4
mariangace <mariangace@gmail.com>
2021-09-04 15:04:27 -0500

    adding Enzyme adapter

---
+++ jotto/src/Congrats.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// receive the success state as a prop
+
+/**
+ * Functional react component for congratory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns { JSX.Element } - Rendered component (or null if success prop is false)
+ */
+
+export default (props) => {
+    if(props.success) {
+    return (
+        <div data-test="component-congrats"> 
+            <span data-test="congrats-message">
+                Congraturalitons! You guessed the word!
+            </span>
+        </div>
+    )} else{
+        return(
+            <div data-test="component-congrats" />
+        )
+        
+    }
+}
